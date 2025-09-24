27c94cfec887a338a62612295af81570e8ea5556
Trần Văn Khởi <tvkhoi123456@gmail.com>
2025-09-24 15:11:22 +0700

    add animaiton load web

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -5,7 +5,7 @@ import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+    provideServerRendering(withRoutes(serverRoutes)),
   ]
 };
 
