0c011b810e9841bb54d86ad8f7c5044afaeda264
Trần Văn Khởi <tvkhoi123456@gmail.com>
2025-09-26 14:15:52 +0700

    fix ui

--- src/app/shared/layouts/admin-layout/admin-layout.ts
+++ src/app/shared/layouts/admin-layout/admin-layout.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
@@ -31,7 +31,7 @@ import { Header } from "../header/header";
   styleUrl: './admin-layout.scss',
   standalone: true,
 })
-export class AdminLayout implements OnInit {
+export class AdminLayout {
   private router = inject(Router);
   private authService = inject(AuthService);
   private messageService = inject(NzMessageService);
@@ -60,42 +60,16 @@ export class AdminLayout implements OnInit {
   ];
 
   menuItemsRight = [
-    { label: 'Thông báo', icon: 'notification', route: '/admin/notifications' },
-    { label: 'Hỗ trợ', icon: 'support', route: '/admin/support' },
+    { label: 'Tài khoản của tôi', icon: 'user', route: '/admin/notifications' },
   ];
-
-  ngOnInit(): void {
-    // this.router.events
-    //   .pipe(filter((event) => event instanceof NavigationEnd))
-    //   .subscribe((event: any) => {
-    //     const path = event.urlAfterRedirects;
-    //     const config = this.routeMap[path];
-    //     if (config) {
-    //       this.currentTitle = config.title;
-    //       this.currentIcon = config.icon;
-    //     }
-    //   });
-  }
 
   onCollapse(collapsed: any): void {
     this.isCollapsed = collapsed;
     this.cdj.detectChanges();
   }
 
-  // logout() {
-  //   this.authService.clearTokens();
-  //   this.router.navigate(['/login']);
-  //   this.messageService.success('Đăng xuất thành công');
-  // }
-
   onToggleProfileCard() {
     this.isProfileCardOpen = !this.isProfileCardOpen;
   }
-  // getEmail(): string {
-  //   return this.authService.getEmail() || 'Chưa có email';
-  // }
 
-  // getName(): string {
-  //   return this.authService.getName() || 'Chưa có tên';
-  // }
 }
