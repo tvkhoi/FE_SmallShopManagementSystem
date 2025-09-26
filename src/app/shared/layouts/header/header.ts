1e29ff66b3f40af9c0c750490c3dd5d62f416db9
Trần Văn Khởi <tvkhoi123456@gmail.com>
2025-09-26 10:46:27 +0700

    fix UI and separate components sidebar-left, sidebar-right, header

---
+++ src/app/shared/layouts/header/header.ts
@@ -0,0 +1,31 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzHeaderComponent } from "ng-zorro-antd/layout";
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  imports: [NzHeaderComponent, NzIconModule, RouterModule],
+  templateUrl: './header.html',
+  styleUrls: ['./header.scss']
+})
+export class Header implements OnInit {
+  private router = inject(Router);
+  @Input() routeMap: { [key: string]: { title: string; icon: string } } = {};
+  @Input() currentTitle: string = '';
+  @Input() currentIcon: string = '';
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const path = event.urlAfterRedirects;
+        const config = this.routeMap[path];
+        if (config) {
+          this.currentTitle = config.title;
+          this.currentIcon = config.icon;
+        }
+      });
+  }
+}
